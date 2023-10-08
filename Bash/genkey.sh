7d894b322a226832223a78792d34d4607b3904d4
Agromahdi123 <32304862+Agromahdi123@users.noreply.github.com>
2023-10-08 15:40:31 -0400

    added ssh helper scripts

---
+++ Bash/genkey.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# Prompt the user to input the username, target server and location to save the key
+read -p "Enter your username: " username
+read -p "Enter your server address: " target_server
+read -p "Enter the location to save the key: " key_location
+
+# Generate the SSH key
+ssh-keygen -t rsa -b 4096 -C "$username@$target_server" -f "$key_location"
+
+# Upload the public key to the target server
+ssh-copy-id -i "$key_location.pub" "$username@$target_server"
+
+# Prompt the user if they want to add more servers
+while true; do
+    read -p "Do you want to add more servers? (y/n): " yn
+    case $yn in
+        [Yy]* ) 
+            read -p "Enter your server address: " target_server
+            read -p "Enter the location to save the key: " key_location
+            ssh-keygen -t rsa -b 4096 -C "$username@$target_server" -f "$key_location"
+            ssh-copy-id -i "$key_location.pub" "$username@$target_server"
+            ;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
---
+++ Bash/sshconfig.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Prompt user for variables
+read -p "Enter the alias: " alias
+read -p "Enter the hostname: " hostname
+read -p "Enter the IP address: " ip
+read -p "Enter the username: " username
+read -p "Enter the port number: " port
+read -p "Enter the location of the identity file: " identity_file
+
+# Generate SSH config file
+echo "Host $alias
+    HostName $hostname
+    User $username
+    Port $port
+    HostKeyAlias $alias
+    CheckHostIP no
+    IdentityFile $identity_file
+    ProxyCommand ssh -q -W %h:%p $ip" >> ~/.ssh/config
+
+echo "SSH config file updated successfully!"
